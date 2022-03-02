779edbcca63c33bf3abff92431869f3c5b94132d
Tomich1 <19tkachenko96@gmail.com>
2022-03-02 12:19:19 +0700

    fix font fix listener

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,98 +1,78 @@
 package com.example.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import net.objecthunter.exp4j.ExpressionBuilder
-import java.lang.Exception
+
+
 
 
 class MainActivity : AppCompatActivity() {
+    
+    
     override fun onCreate(savedInstanceState: Bundle?) {
+        
         super.onCreate(savedInstanceState)
+        
         setContentView(R.layout.activity_main)
-
-        bt0.setOnClickListener { setTextFields("0") }
-        bt1.setOnClickListener { setTextFields("1") }
-        bt2.setOnClickListener { setTextFields("2") }
-        bt3.setOnClickListener { setTextFields("3") }
-        bt4.setOnClickListener { setTextFields("4") }
-        bt5.setOnClickListener { setTextFields("5") }
-        bt6.setOnClickListener { setTextFields("6") }
-        bt7.setOnClickListener { setTextFields("7") }
-        bt8.setOnClickListener { setTextFields("8") }
-        bt9.setOnClickListener { setTextFields("9") }
-        btMinus.setOnClickListener { setTextFields("-") }
-        btPercent.setOnClickListener { setTextFields("%") }
-        btPlus.setOnClickListener { setTextFields("+") }
-        btMultiplie.setOnClickListener { setTextFields("*") }
-        btDivision.setOnClickListener { setTextFields("/") }
-        btComma.setOnClickListener { setTextFields(",") }
-
-
-
-
-        btEquals.setOnClickListener {
-            try {
-                val ex = ExpressionBuilder(txEight.text.toString()).build()
-                val result = ex.evaluate()
-                val longRes = result.toLong()
-                if (result == longRes.toDouble())
-                    txEight.text = longRes.toString()
-                else
-                     txEight.text = result.toString()
-
-            } catch (e: Exception) {
-                Log.d("Ошибка", "сообщение: ${e.message}")
+    
+        val listener = View.OnClickListener { view ->
+            val b = view as Button
+            txEight.append(b.text)
+            
+        }
+        groupButton.referencedIds.forEach { view ->
+            val myView = findViewById<View>(view)
+            
+            myView.setOnClickListener(listener)
+        }
+        
+            btEquals.setOnClickListener {
+                try {
+                    val ex = ExpressionBuilder(txEight.text.toString()).build()
+                    val result = ex.evaluate()
+                    val longRes = result.toLong()
+                    if (result == longRes.toDouble())
+                        txEight.text = longRes.toString()
+                    else {
+                        txEight.text = result.toString()
+                    }
+                    
+                } catch (e: Exception) {
+                
+                }
+            }
+            btNegate.setOnClickListener {
+                try {
+                    val ex = ExpressionBuilder(txEight.text.toString()).build()
+                    val result = ex.evaluate().toInt()
+                    val mark = result * (-1)
+                    txEight.text = mark.toString()
+                    
+                } catch (e: Exception) {
+                
+                }
+            }
+            btPercent.setOnClickListener {
+                try {
+                    val ex = ExpressionBuilder(txEight.text.toString()).build()
+                    val result = ex.evaluate().toInt()
+                    val marker = result / 100
+                    
+                    txEight.text = marker.toString()
+                } catch (e: Exception) {
+                }
+            }
+            
+            btAc.setOnClickListener {
+                txEight.text = ""
             }
         }
-
-        btNegate.setOnClickListener {
-            try {
-                val ex = ExpressionBuilder(txEight.text.toString()).build()
-                val result = ex.evaluate().toInt()
-                val longRes = result.toLong()
-                val mark = result * (-1)
-
-                txEight.text = mark.toString()
-
-
-
-            } catch (e: Exception) {
-                Log.d("Ошибка", "сообщение: ${e.message}")
-            }
-
-        }
-        btPercent.setOnClickListener {
-            try {
-                val ex = ExpressionBuilder(txEight.text.toString()).build()
-                val result = ex.evaluate().toInt()
-                val longRes = result.toLong()
-                val marker = result / 100
-
-                txEight.text = marker.toString()
-
-
-
-            } catch (e: Exception) {
-                Log.d("Ошибка", "сообщение: ${e.message}")
-            }
-        }
-
-
-        btAc.setOnClickListener {
-            txEight.text = ""
-
-        }
-
-
-    }
-
-
-
-    fun setTextFields(resultator: String){
-
-        txEight.append(resultator)
-    }
+    
 }
+
+
+
