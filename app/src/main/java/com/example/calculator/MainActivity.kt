20fe0edb72b85bc606e38cbf13749cc3302ce82d
Tomich1 <19tkachenko96@gmail.com>
2022-03-09 15:06:29 +0700

    fix private val listener added toast

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,76 +3,91 @@ package com.example.calculator
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import net.objecthunter.exp4j.ExpressionBuilder
 
 
-
-
 class MainActivity : AppCompatActivity() {
     
+    private val listener = View.OnClickListener { view ->
+        val b = view as Button
+        txEight.append(b.text)
+    }
     
     override fun onCreate(savedInstanceState: Bundle?) {
         
         super.onCreate(savedInstanceState)
         
         setContentView(R.layout.activity_main)
-    
-        val listener = View.OnClickListener { view ->
-            val b = view as Button
-            txEight.append(b.text)
-            
-        }
+        
+        
         groupButton.referencedIds.forEach { view ->
             val myView = findViewById<View>(view)
+            
             
             myView.setOnClickListener(listener)
         }
         
-            btEquals.setOnClickListener {
-                try {
-                    val ex = ExpressionBuilder(txEight.text.toString()).build()
-                    val result = ex.evaluate()
-                    val longRes = result.toLong()
-                    if (result == longRes.toDouble())
-                        txEight.text = longRes.toString()
-                    else {
-                        txEight.text = result.toString()
-                    }
-                    
-                } catch (e: Exception) {
+        btEquals.setOnClickListener {
+            try {
+                val ex = ExpressionBuilder(txEight.text.toString()).build()
+                val result = ex.evaluate()
+                val longRes = result.toLong()
+                if (result == longRes.toDouble())
+                    txEight.text = longRes.toString()
+                else {
+                    txEight.text = result.toString()
+                }
                 
-                }
-            }
-            btNegate.setOnClickListener {
-                try {
-                    val ex = ExpressionBuilder(txEight.text.toString()).build()
-                    val result = ex.evaluate().toInt()
-                    val mark = result * (-1)
-                    txEight.text = mark.toString()
-                    
-                } catch (e: Exception) {
+            } catch (e: Exception) {
+                val text = "Wrong!"
+                val duration = Toast.LENGTH_SHORT
                 
-                }
-            }
-            btPercent.setOnClickListener {
-                try {
-                    val ex = ExpressionBuilder(txEight.text.toString()).build()
-                    val result = ex.evaluate().toInt()
-                    val marker = result / 100
-                    
-                    txEight.text = marker.toString()
-                } catch (e: Exception) {
-                }
-            }
-            
-            btAc.setOnClickListener {
-                txEight.text = ""
+                val toast = Toast.makeText(applicationContext, text, duration)
+                toast.show()
             }
         }
+        btNegate.setOnClickListener {
+            try {
+                val ex = ExpressionBuilder(txEight.text.toString()).build()
+                val result = ex.evaluate().toInt()
+                val mark = result * (-1)
+                txEight.text = mark.toString()
+                
+            } catch (e: Exception) {
+                val text = "Wrong!"
+                val duration = Toast.LENGTH_SHORT
+                
+                val toast = Toast.makeText(applicationContext, text, duration)
+                toast.show()
+            }
+        }
+        btPercent.setOnClickListener {
+            try {
+                val ex = ExpressionBuilder(txEight.text.toString()).build()
+                val result = ex.evaluate().toInt()
+                val marker = result / 100
+                
+                txEight.text = marker.toString()
+            } catch (e: Exception) {
+                val text = "Wrong!"
+                val duration = Toast.LENGTH_SHORT
+                
+                val toast = Toast.makeText(applicationContext, text, duration)
+                toast.show()
+            }
+        }
+        
+        btAc.setOnClickListener {
+            txEight.text = ""
+        }
+    }
     
 }
+
+
 
 
 
